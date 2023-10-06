c0c4e9ca0c630d458f065439c63ad2de73feb1af
VladSoren <vlad1998ss@mail.ru>
2023-10-06 16:54:18 +0300

    init

---
+++ main.py
@@ -0,0 +1,12 @@
+import uvicorn
+from fastapi import FastAPI
+
+from api_v1 import router as router_v1
+from core.config import settings
+
+app = FastAPI()
+app.include_router(router=router_v1, prefix=settings.api_v1_prefix)
+
+if __name__ == "__main__":
+    # run app on the host and port
+    uvicorn.run("main:app", host="127.0.0.1", port=8001, reload=True)
