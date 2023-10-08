8437ee2f73607a8e783a198fb7092836f529b012
VladSoren <vlad1998ss@mail.ru>
2023-10-08 12:39:47 +0300

    disabled extra data

--- api_v1/event/views.py
+++ api_v1/event/views.py
@@ -1,53 +1,53 @@
-from fastapi import APIRouter, Depends, status
-from sqlalchemy.ext.asyncio import AsyncSession
-
-from api_v1.event import crud
-from api_v1.event.dependencies import driver_by_id
-from api_v1.event.schemas import Driver, DriverCreate
-from api_v1.route.schemas import Route
-from api_v1.user.schemas import Auto
-from core.models import db_helper
-
-router = APIRouter(
-    tags=["Driver"],
-)
-
-
-@router.post("/", response_model=Driver, status_code=status.HTTP_201_CREATED)
-async def create_driver(
-    driver_in: DriverCreate,
-    session: AsyncSession = Depends(db_helper.scoped_session_dependency),
-):
-    return await crud.create_driver(session, driver_in)
-
-
-@router.get("/", response_model=list[Driver])
-async def get_drivers(
-    session: AsyncSession = Depends(db_helper.scoped_session_dependency),
-):
-    return await crud.get_drivers(session=session)
-
-
-@router.get("/{driver_id}/", response_model=Driver)
-async def get_driver(
-    driver: Driver = Depends(driver_by_id),
-):
-    return driver
-
-
-@router.get("/{driver_id}/autos/", response_model=list[Auto])
-async def get_all_driver_autos(
-    driver_id: int,
-    session: AsyncSession = Depends(db_helper.scoped_session_dependency),
-    _: Driver = Depends(driver_by_id),  # check if user is exist
-):
-    return await crud.get_all_driver_autos(session, driver_id)
-
-
-@router.get("/{driver_id}/routes/", response_model=list[Route])
-async def get_all_driver_routes(
-    driver_id: int,
-    session: AsyncSession = Depends(db_helper.scoped_session_dependency),
-    _: Driver = Depends(driver_by_id),  # check if user is exist
-):
-    return await crud.get_all_driver_routes(session, driver_id)
+# from fastapi import APIRouter, Depends, status
+# from sqlalchemy.ext.asyncio import AsyncSession
+#
+# from api_v1.event import crud
+# from api_v1.event.dependencies import driver_by_id
+# from api_v1.event.schemas import Driver, DriverCreate
+# from api_v1.route.schemas import Route
+# from api_v1.user.schemas import Auto
+# from core.models import db_helper
+#
+# router = APIRouter(
+#     tags=["Driver"],
+# )
+#
+#
+# @router.post("/", response_model=Driver, status_code=status.HTTP_201_CREATED)
+# async def create_driver(
+#     driver_in: DriverCreate,
+#     session: AsyncSession = Depends(db_helper.scoped_session_dependency),
+# ):
+#     return await crud.create_driver(session, driver_in)
+#
+#
+# @router.get("/", response_model=list[Driver])
+# async def get_drivers(
+#     session: AsyncSession = Depends(db_helper.scoped_session_dependency),
+# ):
+#     return await crud.get_drivers(session=session)
+#
+#
+# @router.get("/{driver_id}/", response_model=Driver)
+# async def get_driver(
+#     driver: Driver = Depends(driver_by_id),
+# ):
+#     return driver
+#
+#
+# @router.get("/{driver_id}/autos/", response_model=list[Auto])
+# async def get_all_driver_autos(
+#     driver_id: int,
+#     session: AsyncSession = Depends(db_helper.scoped_session_dependency),
+#     _: Driver = Depends(driver_by_id),  # check if user is exist
+# ):
+#     return await crud.get_all_driver_autos(session, driver_id)
+#
+#
+# @router.get("/{driver_id}/routes/", response_model=list[Route])
+# async def get_all_driver_routes(
+#     driver_id: int,
+#     session: AsyncSession = Depends(db_helper.scoped_session_dependency),
+#     _: Driver = Depends(driver_by_id),  # check if user is exist
+# ):
+#     return await crud.get_all_driver_routes(session, driver_id)
