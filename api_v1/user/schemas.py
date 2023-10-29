2210b585701af949ed60b95da84f4cbe561af180
Soren <vlad1998ss@mail.ru>
2023-10-29 21:02:04 +0300

    added None opportunity for UserBase scheme

--- api_v1/user/schemas.py
+++ api_v1/user/schemas.py
@@ -4,12 +4,12 @@ from pydantic import BaseModel, ConfigDict, Field
 
 
 class UserBase(BaseModel):
-    vk_id: str = Field(max_length=100)
-    first_name: str = Field(max_length=100)
-    last_name: str = Field(max_length=100)
-    sex: int = Field(ge=0, le=2)
-    city: str = Field(max_length=100)
-    bdate: datetime.date
+    vk_id: str | None = Field(max_length=100)
+    first_name: str | None = Field(max_length=100)
+    last_name: str | None = Field(max_length=100)
+    sex: int | None = Field(ge=0, le=2)
+    city: str | None = Field(max_length=100)
+    bdate: datetime.date | None
     region_id: int | None = Field(ge=0, default=None)
 
 
