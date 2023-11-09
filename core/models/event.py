2c7ad028ad8f49368747b6998f86071197b2e97b
Soren <vlad1998ss@mail.ru>
2023-11-09 20:25:13 +0300

    Event: deleted extra columns

--- core/models/event.py
+++ core/models/event.py
@@ -1,6 +1,6 @@
 import datetime
 
-from sqlalchemy import DateTime, ForeignKey, String, Text
+from sqlalchemy import DateTime, ForeignKey, String, Text, Integer
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from core.models import Base
@@ -12,10 +12,35 @@ class Event(Base):
     date_time: Mapped[datetime.datetime] = mapped_column(DateTime(), unique=False)
 
     region_id: Mapped[int] = mapped_column(ForeignKey("region.id"))
-    creator_id: Mapped[int] = mapped_column(ForeignKey("creator.id"))
-    speaker_id: Mapped[int] = mapped_column(ForeignKey("speaker.id"))
+    # creator_id: Mapped[int] = mapped_column(ForeignKey("creator.id"))
+    # speaker_id: Mapped[int] = mapped_column(ForeignKey("speaker.id"))
+
+    # creator_id: Mapped[int] = mapped_column(ForeignKey("user.id"), nullable=False)
+    # speaker_id: Mapped[int] = mapped_column(ForeignKey("user.id"), nullable=False)
+
 
     # relationships
     region = relationship("Region", back_populates="event")
-    creator = relationship("Creator", back_populates="event")
-    speaker = relationship("Speaker", back_populates="event")
+    # creator = relationship("Creator", back_populates="event")
+    # speaker = relationship("Speaker", back_populates="event")
+
+
+# class Address(Base):
+#     __tablename__ = "address"
+#     id = mapped_column(Integer, primary_key=True)
+#     street = mapped_column(String)
+#     city = mapped_column(String)
+#     state = mapped_column(String)
+#     zip = mapped_column(String)
+#
+#
+# class Customer(Base):
+#     __tablename__ = "customer"
+#     id = mapped_column(Integer, primary_key=True)
+#     name = mapped_column(String)
+#
+#     billing_address_id = mapped_column(Integer, ForeignKey("address.id"))
+#     shipping_address_id = mapped_column(Integer, ForeignKey("address.id"))
+#
+#     billing_address = relationship("Address", foreign_keys=[billing_address_id])
+#     shipping_address = relationship("Address", foreign_keys=[shipping_address_id])
