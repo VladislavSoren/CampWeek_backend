1fe7a90c0e748e218829c60e8048bd94a21395cb
Soren <vlad1998ss@mail.ru>
2024-01-07 14:02:11 +0300

    feat(Mail): Have made mailings: any any_days_before & any_hours_minutes_before

--- api_v1/mail/auto_event_mail.py
+++ api_v1/mail/auto_event_mail.py
@@ -1,9 +1,28 @@
+"""
+Two mailings:
+- any_days_before
+- any_hours_minutes_before
+
+*any_days_before
+- if days_shift NOT None or 0
+- Get events from DB which will start in set days_shift
+- Make task for mailing
+- Set standard mailing time
+
+*any_hours_minutes_before
+- if days_shift is None or 0
+- Get today events from DB
+- Make task for mailing for every event
+- Set tyme by hours_shift & minutes_shift
+"""
+
 from datetime import datetime, timedelta
 
 import vk_api
 from sqlalchemy import select, Result, and_
 from sqlalchemy.orm import selectinload
 
+from api_v1.mail import crud
 from core.config import ACCESS_MESSAGE_GROUP_TOKEN
 from core.models import db_helper, Event, User
 
@@ -42,8 +61,36 @@ async def get_users_for_event(event):
     return users_list
 
 
-async def send_mail(events):
-    print(f"Рассылка по мероприятию")
+async def send_mail_by_event(event):
+    print(f"send_mail_by_event(event)")
+
+    current_time = datetime.now()
+
+    token = ACCESS_MESSAGE_GROUP_TOKEN
+    vk_session = vk_api.VkApi(token=token)
+    vk = vk_session.get_api()
+
+    message = f"Название - {event.name}\nДата - {event.date_time}\nВремя - {event.time_start}"
+    message = f"{message}\nВремя отправления - {current_time}"
+
+    users = await get_users_for_event(event)
+
+    for user in users:
+        user_id = user.vk_id
+
+        random_id = 0
+        try:
+            response = vk.messages.send(
+                user_id=user_id,
+                random_id=random_id,
+                message=message,
+            )
+        except vk_api.exceptions.ApiError as e:
+            print(e)
+
+
+async def send_mail_by_events(events):
+    print(f"send_mail_by_events(events)")
 
     current_time = datetime.now()
 
@@ -52,6 +99,10 @@ async def send_mail(events):
     vk = vk_session.get_api()
 
     for event in events:
+
+        message = f"Название - {event.name}\nДата - {event.date_time}\nВремя - {event.time_start}"
+        message = f"{message}\nВремя отправления - {current_time}"
+
         users = await get_users_for_event(event)
 
         for user in users:
@@ -62,30 +113,80 @@ async def send_mail(events):
                 response = vk.messages.send(
                     user_id=user_id,
                     random_id=random_id,
-                    message=f"Ping_{current_time}",
+                    message=message,
                 )
             except vk_api.exceptions.ApiError as e:
                 print(e)
 
 
-async def mail_task(days_shift):
-    print("mail_task")
+async def mail_task_any_days_before(days_shift):
+    print("mail_task_any_days_before")
     events = await get_events_by_date(days_shift)
-    await send_mail(events)
+    await send_mail_by_events(events)
+
+
+async def mail_task_any_hours_minutes_before(event):
+    print("mail_task_any_hours_minutes_before")
+    await send_mail_by_event(event)
 
 
 # функция - задание
-async def make_tasks(scheduler, days_shift):
-    # Get events that will start in 2 days
+async def make_tasks(scheduler):
     current_time = datetime.now()
-    task_execute_date = (current_time + timedelta(minutes=1))
     # task_execute_date = (current_time + timedelta(days=days_shift)).replace(hour=0, minute=0, second=0,
     #                                                                         microsecond=0)
 
-    print("Создание задач по данным из бд")
-    # планирование задания
-    scheduler.add_job(
-        mail_task,
-        args=[days_shift], trigger='date',
-        run_date=task_execute_date.strftime("%Y-%m-%d %H:%M:%S"))
-    scheduler.print_jobs()
+    # Ставим задачи по авторассылкам по мероприятиям из БД
+    print("Получаем задачи из БД из бд")
+    async with db_helper.async_session_factory() as session:
+        auto_event_mails = await crud.get_auto_event_mails(session=session)
+
+        for mail_task_id, auto_event_mail in enumerate(auto_event_mails):
+
+            # Выставление стандартного времени
+            task_execute_date = (current_time + timedelta(minutes=1))
+
+            # Если указан сдвиг по дню days_shift, то выставляем рсасылку на стандартное время,
+            # которые будут, через указанное кол-ов дней, БЕЗ УЧЁТА ДРУГИХ СДВИГОВ
+            if auto_event_mail.days_shift is not None and auto_event_mail.days_shift != 0:
+                scheduler.add_job(
+                    mail_task_any_days_before,
+                    args=[auto_event_mail.days_shift], trigger='date',
+                    run_date=task_execute_date.strftime("%Y-%m-%d %H:%M:%S"),
+                    misfire_grace_time=60,  # sec
+                )
+                scheduler.print_jobs()
+            # Если кол-во дней НЕ указано - проверяем все мероприятия на текущий день
+            # Выставляем время рассылки по сдвигам hours_shift, minutes_shift
+            else:
+                # Постановка задач для рассылки для каждого мероприятия за определённое время до начала
+                events_today = await get_events_by_date(0)
+
+                for event_id, event in enumerate(events_today):
+                    time_start = event.time_start
+                    hour_start, min_start = tuple(time_start.split(":"))
+
+                    event_start_datetime = datetime(
+                        current_time.year,
+                        current_time.month,
+                        current_time.day,
+                        int(hour_start),
+                        int(min_start)
+                    )
+
+                    task_execute_date = (event_start_datetime - timedelta(
+                        hours=int(auto_event_mail.hours_shift or 0),
+                        minutes=int(auto_event_mail.minutes_shift or 0)
+                    ))
+
+                    job_id = f"mail_task_any_hours_minutes_before_{mail_task_id}{event_id}_{event.name}"
+                    scheduler.add_job(
+                        mail_task_any_hours_minutes_before,
+                        args=[event], trigger='date',
+                        run_date=task_execute_date.strftime("%Y-%m-%d %H:%M:%S"),
+                        misfire_grace_time=300,
+                        replace_existing=True,
+                        id=job_id
+                    )
+                scheduler.print_jobs()
+
--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from datetime import datetime, timedelta
+
 import uvicorn
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from fastapi import FastAPI
@@ -8,7 +10,11 @@ from api_v1 import router as router_v1
 from api_v1.mail.auto_event_mail import make_tasks
 from core.config import settings
 
+import logging
 
+# logging settings
+logging.basicConfig()
+logging.getLogger('apscheduler').setLevel(logging.DEBUG)
 
 app = FastAPI()
 app.include_router(router=router_v1, prefix=settings.api_v1_prefix)
@@ -30,17 +36,27 @@ scheduler = AsyncIOScheduler()
 @app.on_event("startup")
 async def load_schedule_or_create_blank():
     global scheduler
-    try:
-        days_shift = 2
 
+    current_time = datetime.now()
+    task_execute_date = (current_time + timedelta(
+        minutes=1
+    ))
+
+    try:
         # jobstores = {
         # 'default': SQLAlchemyJobStore(url='sqlite:///jobs.sqlite')
         # }
         # Schedule = AsyncIOScheduler(jobstores=jobstores)
         scheduler = AsyncIOScheduler()
+        # scheduler.add_job(
+        #     make_tasks, args=[scheduler], id='trigger_task', trigger='cron', minute='*/15',
+        #     start_date=task_execute_date.strftime("%Y-%m-%d %H:%M:%S")
+        # )
         scheduler.add_job(
-            make_tasks, args=[scheduler, days_shift], id='trigger_task', trigger='cron', minute='*/3')
+            make_tasks, args=[scheduler], id='trigger_task', trigger='cron',  hour=13, minute=37
+        )
         scheduler.start()
+        pass
     except Exception as e:
         print(e)
 
@@ -51,7 +67,6 @@ def index():
 
 
 if __name__ == "__main__":
-    # # run app on the host and port
-    uvicorn.run("main:app", host="0.0.0.0", port=5777, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=5777, reload=False, log_level="debug")
     # uvicorn.run("main:app", host="0.0.0.0", port=5777, reload=False, log_level="debug",
     #             workers=1, limit_concurrency=1, limit_max_requests=1)
